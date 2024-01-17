0b3e3bc64032e31f6c8f2b6f09470a14abad7110
=Riky <=ra_mo116@hotmail.com>
2024-01-17 08:37:57 -0800

    multiple style changes

--- src/assets/cakesdata.js
+++ src/assets/cakesdata.js
@@ -3,45 +3,50 @@
       "id": 1,  
       "type": "classic",
       "event" : "birthday",
-      "name": "Luca",  
+      "name": "Mickey Mouse",  
       "flavor": "chocolate",  
       "size": "medium" ,
-      "image": "../../assets/img/birthdaycake.jpg" 
+      "image": "../../assets/img/birthdaycake.jpg" ,
+      "description": "White and blue cake with fondant decoration"
     },  
     {  
       "id": 2,  
       "type": "classic",
       "event" : "birthday",
-      "name": "Lilly",  
+      "name": "Flowers",  
       "flavor": "vanilla",  
       "size": "medium" ,
-      "image": "../../assets/img/birthdaycake4.jpg"  
+      "image": "../../assets/img/birthdaycake4.jpg",
+      "description": "Classic cake with yellow flowers on top"  
     },  
     {  
       "id": 3,  
       "type": "classic",
       "event" : "birthday",
-      "name": "Anna",  
+      "name": "Frida Calo",  
       "flavor": "moca",  
-      "size": "small"  ,
-      "image": "../../assets/img/birthdaycake5.jpg" 
+      "size": "large"  ,
+      "image": "../../assets/img/birthdaycake5.jpg" ,
+      "description": "Multi-color cake with flowers"
     },  
     {  
       "id": 4,  
       "type": "classic",
       "event" : "birthday",
-      "name": "John",  
+      "name": "COVID-19",  
       "flavor": "strawberry",  
-      "size": "large"  ,
-      "image": "../../assets/img/birthdaycake3.jpg" 
+      "size": "small" ,
+      "image": "../../assets/img/birthdaycake3.jpg" ,
+      "description": "Angry covid face"
     },  
     {  
       "id": 5,  
       "type": "classic",
       "event" : "birthday",
-      "name": "Mary",  
+      "name": "15th Anniversary",  
       "flavor": "red velvet",
-      "size": "medium"  ,
-      "image": "../../assets/img/birthdaycake6.jpg" 
+      "size": "large"  ,
+      "image": "../../assets/img/birthdaycake6.jpg",
+      "description": "Beautiful 15th Anniversary cake which contains different size cakes"
     }  
   ]  
