2860eb7da4f77fedf09a5d918392238b0f893aaf
=Riky <=ra_mo116@hotmail.com>
2025-03-19 10:52:03 -0700

    new vehicle section added

--- src/app/newvehicle/newvehicle.component.ts
+++ src/app/newvehicle/newvehicle.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Vehicle } from '../vehicle';
 
 interface Year {
   value: string;
   viewValue: string;
 }
 
+interface Make {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-newvehicle',
@@ -15,11 +19,27 @@ interface Year {
 
 
 export class NewvehicleComponent {
-
+    carmodel !: string;
+    selectedyear !: string;
+    selectedmake !: string;
   years: Year[] = [
     {value: '1999', viewValue: '1999'},
     {value: '2000', viewValue: '2000'},
     {value: '2001', viewValue: '2001'},
   ];
+
+  makes: Make[] = [
+    {value: 'Ford', viewValue: 'Ford'},
+    {value: 'Nissan ', viewValue: 'Nissan'},
+    {value: 'Mazda', viewValue: 'Mazda'},
+  ];
+
+  showVehicle(){
+    if (this.carmodel && this.selectedyear && this.selectedmake ){
+      var vehic = new Vehicle(this.carmodel, this.selectedyear, this.selectedmake);
+      alert (vehic.model + " "+ vehic.year + " "+ vehic.make);
+    }
+
+  }
     
 }
--- src/app/vehicle.ts
+++ src/app/vehicle.ts
@@ -3,11 +3,12 @@ export class Vehicle {
     
     model !:string;
     year !: string;
+    make !: string;
 
-
-    constructor(  model: string, year: string) {
+    constructor(  model: string, year: string, make: string) {
         this.year = year;
         this.model = model;
+        this.make = make;
       }
     
       method(): string {
